805ef6be6e0159b678aac97b7f3514dcba610b98
Busra Artug <busra.artug@bites.com.tr>
2024-01-31 16:43:41 +0300

    ilkmaddeeklendi

---
+++ src/main/java/tr/com/busra/artug/Generic.java
@@ -0,0 +1,11 @@
+package tr.com.busra.artug;
+
+public class Generic<T> {
+    T obj;
+    Generic(T obj) { this.obj = obj; } // constructor
+    public T getObject() { return this.obj; }
+
+
+    }
+
+
--- src/main/java/tr/com/busra/artug/Main.java
+++ src/main/java/tr/com/busra/artug/Main.java
@@ -4,6 +4,20 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+
+
+        //Generic bir alan içeren sınıf ve bu alanı yazdıran bir metot için;
+
+        Generic<Integer> iObj = new Generic<Integer>(15);
+        System.out.println(iObj.getObject());
+        Generic<String> sObj
+                = new Generic<String>("GeeksForGeeks");
+        System.out.println(sObj.getObject());
+
+        //Bir koleksiyonu ve bulunacak bir öğeyi kabul eden bir metod yazın. Metod, eğer böyle bir öğe yoksa
+        //-1 değerini, öğe varsa öğeyi döndürmelidir.
+
+
         ArrayList<Integer> myCollection = new ArrayList<>();
         myCollection.add(1);
         myCollection.add(3);
